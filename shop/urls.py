8c2d78ff3c6617a9f1388fffacb2ff58957992aa
Yassine Hamza <yassine.hamza@esprit.tn>
2024-10-28 13:53:52 +0200

    crud rest

--- shop/urls.py
+++ shop/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/categories', views.category_list, name='category_list'),
-    path('api/categories/<int:category_id>', views.category_detail, name='category_detail'),
-    path('api/games', views.game_list, name='game_list'),
-    path('api/games/<int:game_id>', views.game_detail, name='game_detail'),
+    path('game-categories', views.category_list, name='category_list'),
+    path('game-categories/<int:category_id>', views.category_detail, name='category_detail'),
+    path('game', views.game_list, name='game_list'),
+    path('game/<int:game_id>', views.game_detail, name='game_detail'),
 ]
