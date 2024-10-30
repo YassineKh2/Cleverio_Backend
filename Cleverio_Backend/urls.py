e4efe51c39ff7c404ed2fbe11fc4383ae32bbd83
Yassine Hamza <yassine.hamza@esprit.tn>
2024-10-30 03:24:15 +0200

    commit

--- Cleverio_Backend/urls.py
+++ Cleverio_Backend/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include 
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -25,4 +26,4 @@ urlpatterns = [
     path('api/', include('shop.urls')),
 
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
