a762131a0484e577b2fed319f324b1a6047d111d
malekbnsaid <malek.bensaid@esprit.tn>
2024-10-31 06:48:14 +0100

    crud art+comment

--- artForum/urls.py
+++ artForum/urls.py
@@ -6,10 +6,11 @@ urlpatterns = [
     path('arts/create/', views.create_art, name='create_art'),
     path('arts/<int:art_id>/', views.detail_art, name='detail_art'),
     path('arts/<int:art_id>/update/', views.update_art, name='update_art'),
+    path('arts/<int:art_id>/delete/', views.delete_art, name='delete_art'),  # Changed this to /delete/
 
-    path('arts/<int:art_id>', views.delete_art, name='delete_art'),
-    
-     path('arts/<int:art_id>/comments/', views.list_comments, name='list_comments'),
+    path('arts/<int:art_id>/comments/', views.list_comments, name='list_comments'),
     path('arts/<int:art_id>/comments/create/', views.create_comment, name='create_comment'),
     path('arts/<int:art_id>/comments/<int:comment_id>/delete/', views.delete_comment, name='delete_comment'),
+
+
 ]
--- artForum/views.py
+++ artForum/views.py
@@ -6,6 +6,8 @@ from rest_framework.response import Response
 from .models import Art, Comment
 from .serializers import ArtSerializer, CommentSerializer
 
+
+
 # Create Art
 @api_view(['POST'])
 def create_art(request):
@@ -15,12 +17,14 @@ def create_art(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 # Read Art (List)
 @api_view(['GET'])
 def list_arts(request):
     arts = Art.objects.all()
     serializer = ArtSerializer(arts, many=True)
     return Response(serializer.data)
+
 
 # Read Art (Detail)
 @api_view(['GET'])
@@ -29,9 +33,10 @@ def detail_art(request, art_id):
         art = Art.objects.get(id=art_id)
     except Art.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-    
+
     serializer = ArtSerializer(art)
     return Response(serializer.data)
+
 
 # Update Art
 @api_view(['PUT'])
@@ -40,12 +45,13 @@ def update_art(request, art_id):
         art = Art.objects.get(id=art_id)
     except Art.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-    
+
     serializer = ArtSerializer(art, data=request.data, partial=True)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 # Delete Art
 @api_view(['DELETE'])
@@ -55,16 +61,12 @@ def delete_art(request, art_id):
         art.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
     except Art.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+        return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
 
-# Comment Functions
-@api_view(['POST'])
-def create_comment(request, art_id):
-    serializer = CommentSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save(art_id=art_id)  # Associate with the art piece
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
 
 @api_view(['GET'])
 def list_comments(request, art_id):
@@ -72,13 +74,25 @@ def list_comments(request, art_id):
     serializer = CommentSerializer(comments, many=True)
     return Response(serializer.data)
 
-# Delete Comment
+
 @api_view(['DELETE'])
 def delete_comment(request, art_id, comment_id):
     try:
         comment = Comment.objects.get(id=comment_id, art_id=art_id)
     except Comment.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
+        return Response(status=status.HTTP_404_NOT_FOUND)  # This is correct
+
     comment.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+    return Response(status=status.HTTP_204_NO_CONTENT)  # Ensure this line is indented properly
+
+
+# Comment Functions
+@api_view(['POST'])
+def create_comment(request, art_id):
+    data = request.data.copy()
+    data['art'] = art_id  # Automatically set the art field from the URL parameter
+    serializer = CommentSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
