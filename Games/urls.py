64938b27d0aa3f6c3ec3981a9e4c41e03757ca31
YassineKh2 <yassinkhemiri02@gmail.com>
2024-10-31 10:21:48 +0100

    Added Questions

--- Games/urls.py
+++ Games/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('quiz', views.quiz_list, name='quiz_list'),
     path('quiz/<int:Quizid>', views.singlequiz, name='singlequiz'),
+    path('question', views.question_list, name='question_list'),
 ]
